6e3e0c63b628eff01a39d972516a7dd5a8b71d79
laurastephaniecortes <laurastephaniecortes@gmail.com>
2021-04-25 18:23:19 -0400

    adding add credits button

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component } from 'react';
 import styles from './debitCreditStyles.css';
 export default class Credits extends Component {
   componentDidMount() {
     console.log(this.props.credits);
   }
 
+
   // this.props.addcreditsArray(description, amount, date)
   //use ^^^^^ to add new items to the list
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  let input = event.target;
+  let amountInput = input.input.value;
+  let descriptionInput = input.description.value;
+
+  const currentMonth1 = new Date().getMonth() + 1;
+  const currentMonth = currentMonth1.toString();
+  const currentDay = new Date().getDate().toString();
+  const currentYear = new Date().getFullYear().toString();
+
+  const currentDate = currentMonth + '/' +  currentDay + '/' + currentYear;
+
+  this.props.addCreditsArray(descriptionInput, amountInput, currentDate);
+}
   render() {
     let accountBalance = this.props.accountBalance;
     let creditTransactions = this.props.credits;
@@ -24,6 +41,19 @@ export default class Credits extends Component {
               </ul>
             );
           })}
+          <form onSubmit={this.handleSubmit}>
+            <label>
+               New Credit:
+          <input type="text" name="input" placeholder="amount $"/>
+          </label>
+
+            <input type="text" name="description" placeholder="description"/>
+          
+            <input type="submit" value="Submit" />
+      
+          </form>
+
+ 
         </div>
         <h4 className='account-balance-header'>
           AccountBalance:
